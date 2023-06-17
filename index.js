06ec40aec3b01162443d317a212e3147b4f4ae9c
Irakli Svanidze <svanidzeee@gmail.com>
2023-06-17 11:26:25 +0300

    connect db

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
